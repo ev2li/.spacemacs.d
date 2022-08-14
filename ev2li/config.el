d158dec2a2f063592c66a9850bbbf810584ff1fa
ev2li <ev2li9748@163.com>
2022-08-14 11:28:55 +0800

    add

--- ev2li/config.el
+++ ev2li/config.el
@@ -2,7 +2,7 @@
 (setq-default evil-escape-delay 0.2)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
+;;(setq indent-line-function 'insert-tab)
 (setq auto-save-default nil)
 (setq c-basic-offset 4)
 (setq evil-want-fine-undo t)
