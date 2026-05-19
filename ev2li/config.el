b5663ac648d85fa5f0aef920916efa2c837fb6e3
ev2li <ev2li9748@163.com>
2026-05-19 15:51:32 +0800

    add config

--- ev2li/config.el
+++ ev2li/config.el
@@ -1,5 +1,3 @@
-(setq-default evil-escape-key-sequence "jk")
-(setq-default evil-escape-delay 0.2)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 ;;(setq indent-line-function 'insert-tab)
@@ -8,7 +6,7 @@
 (setq evil-want-fine-undo t)
 (setq byte-compile-warnings '(cl-functions))
 (setq inhibit-compacting-font-caches t)
-(setq projectile-enable-caching t) 
+(setq projectile-enable-caching t)
 (setq spacemacs-show-trailing-whitespace nil)
 (setq max-lisp-eval-depth 1000)
 
